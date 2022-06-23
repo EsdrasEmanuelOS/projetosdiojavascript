57135f617a8c7eca37c6631e178baa8aff47bce9
EsdrasEmanuelOS <108008968+EsdrasEmanuelOS@users.noreply.github.com>
2022-06-23 18:26:36 -0300

    adicionando condição de repetição
    entendendo as condições de repetição
    for
    for/in
    for/of
    while
    do/while

--- aula 4 - estruturas condicionais/script.js
+++ aula 4 - estruturas condicionais/script.js
@@ -1,3 +1,4 @@
+/*
 // estruturas de decisão 
 var jogador1 = 0;
 var jogador2 = 01;
@@ -32,5 +33,46 @@ switch(placar){
       break;
   default:
         console.log('Empate')
-}
-  
+} */
+/* condição de repetição 
+ for: repetição até que a condição seja falsa
+ for/in executa repetição a partir de uma propriedade
+for/of funciona como uma repetição a partir de um valor (não funciona com object somente numeros)
+while (enquanto) a condição for verdadeira ele executa, sempre verifica antes 
+do/while (até que) executa até a condição ser falsa
+*/
+let array = ['valor1 ', 'valor2', 'valor3', 'valor4', 'valor5', ];
+let object = {propriedade1: 'valor1', propriedade2: 'valor2', propriedade3: 'valor3', }
+
+/*for (let indice = 0; indice < array.length; indice++ ) {
+    console.log(indice); 
+}*/
+
+// for (let i in array ) {
+//   console.log(i);
+// }
+
+// // com object
+// for (i in object){
+//   console.log(i);
+// }
+
+// for(i of array){
+//   console.log(i);
+// }
+//  // com object, não recomendado
+// for (i of object.propriedade1){
+//   console.log(i);
+// }
+
+// var a = 0;
+//   while(a<10){
+//      a++;
+//      console.log(a);
+//   }
+
+var a= 0;
+  do{
+      a++;
+      console.log(a);
+  }while (a<10)
